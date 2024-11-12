40c62767a19732e58425dae35115c48b23ff8f53
Tizz27 <101568009+Tizz27@users.noreply.github.com>
2024-11-11 23:35:57 -0300

    Add files via upload

---
+++ Fase 2/proyecto/panaderia/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Producto
+from .models import Cliente
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model= User
+        fields= ['username', "first_name","email", "password1", "password2"]        
+
+class ProductoForm(forms.ModelForm):
+    
+    class Meta:
+        model =Producto
+        fields = '__all__'
+
+class ClienteForm(forms.ModelForm):
+    class Meta:
+        model = Cliente
+        fields = ['nombre_completo', 'fecha_nacimiento', 'email', 'contraseña', 'direccion', 'rut']
+        widgets = {
+            'fecha_nacimiento': forms.DateInput(attrs={'type': 'date'}),
+            'contraseña': forms.PasswordInput(),
+        }
